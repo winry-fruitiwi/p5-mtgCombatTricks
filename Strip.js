52ff3bf37f583553fbaa8f0aa8e3ddcc61f7f09c
Winry <winry.tian@gmail.com>
2023-03-02 17:55:51 -0800

    add new show function that looks more like the one in mtgPrimer

--- Strip.js
+++ Strip.js
@@ -25,7 +25,7 @@ class Strip {
     }
 
     // renders the strip with SVGs, to be implemented much later.
-    show() {
+    originalShow() {
         // a list of colors for each color represented in the strip
         let colors = [
             color(266, 7, 60), // c
@@ -51,8 +51,12 @@ class Strip {
         let radius = 15
 
         // the x-margin between each color representation and the next. There
-        // will be no y-margin.
+        // will be no color selector wrapping unless people use this on their
+        // iPod and iPhones.
         let xMargin = radius + 10
+
+        // the y-margin under the color selectors that I want to erase.
+        let yMargin = 10
 
         // // how rounded the square will be (obsolete)
         // let rounding = 10
@@ -64,14 +68,17 @@ class Strip {
         // the keys of the strip dictionary
         let stripDictKeys = Object.keys(this.stripDict)
 
-        // add a rectangle behind the strip dictionary key display to make it
-        // look like it's part of the background
-        stroke(237, 37, 20)
-        fill(237, 37, 20)
-        strokeWeight(20)
+        // // add a rectangle behind the strip dictionary key display to make it
+        // // look like it's part of the background (obsolete)
+        // stroke(237, 37, 20)
+        // fill(237, 37, 20)
+        // strokeWeight(20)
+
         // the y-position should end exactly where the color selectors
         // vertically end
-        rect(0, 0, width, startY + radius)
+        erase()
+        rect(0, 0, width, startY + radius + yMargin)
+        noErase()
 
         noFill()
         noStroke()
@@ -113,6 +120,110 @@ class Strip {
 
             // draw the circle
             circle(startX + (radius + xMargin) * i, startY, radius * 2)
+        }
+
+        // reset the tint
+        noTint()
+    }
+
+    show() {
+        // a list of colors for each color represented in the strip
+        let colors = [
+            [266, 7, 60], // c
+            [52, 15, 89], // w
+            [218, 45, 85], // u
+            [259, 13, 47], // b
+            [9, 69, 85], // r
+            [89, 100, 68] // g
+        ]
+
+        // a list of mana symbols for each color represented in Strip
+        let manaSymbolImages = [
+            c,
+            w,
+            u,
+            b,
+            r,
+            g
+        ]
+
+        // half of the length of the edge of the square so I can calculate
+        // margin properly
+        let radius = 15
+
+        // the x-margin between each color representation and the next. There
+        // will be no color selector wrapping unless people use this on their
+        // iPod and iPhones.
+        let xMargin = radius + 10
+
+        // the y-margin under the color selectors that I want to erase.
+        let yMargin = 10
+
+        // // how rounded the square will be (obsolete)
+        // let rounding = 10
+
+        // the starting x- and y-position of the dots
+        let startX = radius + 1
+        let startY = radius + 1
+
+        // the keys of the strip dictionary
+        let stripDictKeys = Object.keys(this.stripDict)
+
+        // // add a rectangle behind the strip dictionary key display to make it
+        // // look like it's part of the background (obsolete)
+        // stroke(237, 37, 20)
+        // fill(237, 37, 20)
+        // strokeWeight(20)
+
+        // the y-position should end exactly where the color selectors
+        // vertically end
+        erase()
+        rect(0, 0, width, startY + radius + yMargin)
+        noErase()
+
+        noFill()
+        noStroke()
+
+        // set the stroke weight
+        strokeWeight(2)
+
+        // iterate through the dictionary using its length
+        for (let i = 0; i < stripDictKeys.length; i++) {
+            // draw a circle. x-coordinate should be startingX + (r + xMargin)*i
+            // y-coordinate should be startingY
+            // set the color. if the color isn't selected, make the circle
+            // hollow and transparent instead.
+            let manaImg = manaSymbolImages[i]
+
+            manaImg.resize(radius*1.5, 0)
+
+            if (!this.colorSelected(stripDictKeys[i])) {
+                // tint transparent black
+                tint(0, 0, 0, 20)
+
+                // set a transparent black stroke
+                stroke(0, 0, 0, 20)
+
+                // set a transparent but still colored fill
+                fill(colors[i][0], colors[i][1], colors[i][2], 20)
+            } else {
+                tint(0, 0, 0, 100)
+
+                // set black stroke
+                stroke(0, 0, 0)
+
+                // set colored fill
+                fill(colors[i][0], colors[i][1], colors[i][2])
+            }
+
+            // draw the circle
+            circle(startX + (radius + xMargin) * i, startY, radius * 2)
+
+            // draw the current mana symbol image in the center of the
+            // current color selector
+            image(manaImg,
+                startX + (radius + xMargin) * i - manaImg.width/2,
+                startY - manaImg.height/2)
         }
 
         // reset the tint
